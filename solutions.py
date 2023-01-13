3d3d27e7bb95a299a9432f1ceb1be7ac0fb860dd
eunsang_lee <thecynicdog0328@gmail.com>
2023-01-13 08:59:10 -0500

    init

--- solutions.py
+++ solutions.py
@@ -6,8 +6,6 @@ import collections
 class Solution: 
 
 # [LEETCODE #125. VALID PALINDROME]
-# Given a string s, return true if it is a palindrome, or false otherwise.
-
 	def isPalindrome(self, s: str) -> bool: 
 		
 		strs = [] 
@@ -24,8 +22,6 @@ class Solution:
 
 
 # [LEETCODE #344. REVERSE STRING]
-# Write a function that reverses a string. The input string is given as an array of characters s.
-
 	def reverseString(self, s: list[str]) -> list[str]: 
 		
 		left, right = 0, len(s) - 1 
@@ -49,9 +45,57 @@ class Solution:
 		return counts.most_common(1)[0][0]
 
 
+# [LEETCODE #49. GROUP ANAGRAM]
+	def groupAnagram(self, strs: list[str]) -> list[list[str]]: 
+		
+		anagrams = collections.defaultdict(list)
+	
+		for word in strs: 
+			anagrams[''.join(sorted(word))].append(word) 
+
+		return list(anagrams.values())
+
+
+# [LEETCODE #5. LONGEST PALINDROME] 
+	def longestPalindrome(self, s: str) -> str: 
+		
+		def expand(left: int, right: int) -> str: 
+			
+			while left >= 0 and right < len(s) and s[left] == s[right]: 
+
+				left -= 1  
+				right += 1 
+
+			return s[left + 1 : right] 
+
+		if len(s) < 2 or s == s[::-1]:
+
+			return s
+
+		result = '' 
+		for i in range(len(s) - 1): 
+			result = max(result, expand(i, i + 1), expand(i, i + 2), key = len) 
+
+		return result 
+
+
+# [LEETCODE #1. Two Sum] 
+	def twoSum(self, nums: list[int], target: int) -> list[int]: 
+
+		for idx, num  in enumerate(nums): 
+			complement = target - num
+			
+			if complement in nums: 
+				return [idx, nums[idx + 1:].index(complement) + (idx + 1)]
+
+
+
+
 
 # TEST CASE 
 solution = Solution() 
+
 print(solution.isPalindrome("A man, a plan, a canal: Panama")) 
 print(solution.reverseString(['h', 'e', 'l', 'l', 'o']))  
 print(solution.mostCommonWord('Bob hit a ball, the hit BALL flew far after it was hit.', 'hit'))
+print(solution.longestPalindrome('asdlkaasndssssslkkknaawww'))
