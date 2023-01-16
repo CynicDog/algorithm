dae787590b43b8ae37410e7611163468b2735915
eunsang_lee <thecynicdog0328@gmail.com>
2023-01-16 09:36:02 -0500

    leetcode #1, #42
    Two Sum, Trapping Rain Water

--- solutions.py
+++ solutions.py
@@ -82,13 +82,38 @@ class Solution:
 # [LEETCODE #1. Two Sum] 
 	def twoSum(self, nums: list[int], target: int) -> list[int]: 
 
-		for idx, num  in enumerate(nums): 
-			complement = target - num
+		numsMap = {}
+
+		for idx, num in enumerate(nums): 
+			if target - num in numsMap: 
+				return [idx, numsMap[target - num]]
+
+			numsMap[num] = idx 
+
+
+# [LEETCODE #42. TRAPPING RAIN WATER]
+	def trap(self, height: list[int]) -> int: 
+
+		if not height: 
+			return 0 
+
+		volume = 0 
+		left, right = 0, len(height) - 1 
+
+		left_max, right_max = height[left], height[right] 
+
+		while left < right:
+			left_max, right_max = max(left_max, height[left]), max(right_max, height[right])
 			
-			if complement in nums: 
-				return [idx, nums[idx + 1:].index(complement) + (idx + 1)]
+			if left_max <= right_max: 
+				volume += left_max - height[left] 
+				left += 1 
 
+			else:
+				volume += right_max - height[right] 
+				right -= 1 
 
+		return volume 
 
 
 
@@ -99,3 +124,5 @@ print(solution.isPalindrome("A man, a plan, a canal: Panama"))
 print(solution.reverseString(['h', 'e', 'l', 'l', 'o']))  
 print(solution.mostCommonWord('Bob hit a ball, the hit BALL flew far after it was hit.', 'hit'))
 print(solution.longestPalindrome('asdlkaasndssssslkkknaawww'))
+print(solution.twoSum(nums = [3, 2, 4], target = 6))
+print(solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
