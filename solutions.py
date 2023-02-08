4152dd538c867155ad048f33f51a560250b427ed
eunsang_lee <thecynicdog0328@gmail.com>
2023-02-08 11:32:37 -0500

    docs, leetcode #543

--- solutions.py
+++ solutions.py
@@ -12,6 +12,12 @@ class ListNode:
 		self.val = val
 		self.next = next
 
+class TreeNode: 
+    def __init__(self, val = 0, left = None, right = None): 
+        self.val = val
+        self.left = left 
+        self.right = right 
+
 # Soultions 
 class Solution: 
 
@@ -707,3 +713,56 @@ class Solution:
 
 		return -1 
 
+# [LEETCODE #104 MAXIMUM DEPTH OF BINARY TREE]
+	def maxDepth(self, root: Optional[TreeNode]) -> int:
+		  
+		if root is None:
+			return 0 
+
+		queue = collections.deque([root]) 
+		depth = 0 
+
+		while queue: 
+			depth += 1 
+
+			for _ in range(len(queue)): 
+				cur_root = queue.popleft() 
+
+				if cur_root.left:
+					queue.append(cur_root.left) 
+				if cur_root.right:
+					queue.append(cur_root.right) 
+
+		return depth 
+	
+	def maxDepthRecursive(self, root: Optional[TreeNode]) -> int: 
+		def dfs(node, depth):
+			if node is None:
+				return depth 
+
+			depth += 1 
+
+			return max(dfs(node.left, depth), dfs(node.right, depth)) 
+
+		
+		return dfs(root, 0) 
+ 
+
+# [LEETCODE #543 DIAMETER OF BINARY TREE] 
+	path : int = 0
+	def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+		def dfs(node):
+			if node is None: 
+				return 0 
+
+			left = dfs(node.left) 
+			right = dfs(node.right) 
+
+			self.path = max(self.path, left + right) 
+
+			return max(left, right) + 1 
+
+		dfs(root) 
+
+		return self.path 
+			
--- tests.py
+++ tests.py
@@ -12,43 +12,49 @@ class ListNode:
         self.val = val
         self.next = next
 
+class TreeNode: 
+    def __init__(self, val = 0, left = None, right = None): 
+        self.val = val
+        self.left = left  
+        self.right = right 
 
 # Test Cases 
 solution = solutions.Solution()
 
 # [LEETCODE #125. VALID PALINDROME]
-print(solution.isPalindrome("A man, a plan, a canal: Panama"))
+print("#125\t", solution.isPalindrome("A man, a plan, a canal: Panama"))
 
 # [LEETCODE #344. REVERSE STRING]
-print(solution.reverseString(['h', 'e', 'l', 'l', 'o']))
+print("#344\t", solution.reverseString(['h', 'e', 'l', 'l', 'o']))
 
 # [LEETCODE #819. MOST COMMON WORD]
-print(solution.mostCommonWord('Bob hit a ball, the hit BALL flew far after it was hit.', 'hit'))
+print("#819\t", solution.mostCommonWord('Bob hit a ball, the hit BALL flew far after it was hit.', 'hit'))
 
 # [LEETCODE #5. LONGEST PALINDROME]
-print(solution.longestPalindrome('asdlkaasndssssslkkknaawww'))
+print("#5\t", solution.longestPalindrome('asdlkaasndssssslkkknaawww'))
 
 # [LEETCODE #1. TWO SUM] 
-print(solution.twoSum(nums = [3, 2, 4], target = 6))
+print("#1\t", solution.twoSum(nums = [3, 2, 4], target = 6))
 
 # [LEETCODE #42. TRAPPING RAIN WATER]
-print(solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+print("#42\t", solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
 
 # [LEETCODE #15 THREE SUM]
-print(solution.threeSum(nums = [-1, 1, 3, -2, 2, -3]))
+print("#15\t", solution.threeSum(nums = [-1, 1, 3, -2, 2, -3]))
 
 # [LEETCODE #238 PRODUCT EXCEPT SELF]
-print(solution.productExceptSelf([1, 2, 3, 4, 5]))
+print("#238\t", solution.productExceptSelf([1, 2, 3, 4, 5]))
 
 # [LEETCODE #121 MAX PROFIT(1)]
-print(solution.maxProfit([7, 1, 5, 3, 6, 4]))
+print("#121\t", solution.maxProfit([7, 1, 5, 3, 6, 4]))
 
 # [LEETCODE #234 PALINDROME LINKED LIST]
-print(solution.isLinkedListPalindrome(head = ListNode(1, ListNode(2, ListNode(3,None)))))
-print(solution.isLinkedListPalindrome_2(head = ListNode(1, ListNode(2, ListNode(1,None)))))
+print("#234\t", solution.isLinkedListPalindrome(head = ListNode(1, ListNode(2, ListNode(3,None)))))
+print("#234\t", solution.isLinkedListPalindrome_2(head = ListNode(1, ListNode(2, ListNode(1,None)))))
 
 # [LEETCODE #21 MERGE TWO SORTED LISTS]
 merged = solution.mergedTwoLists(ListNode(1, ListNode(2, ListNode(4, None))), ListNode(3, ListNode(5, ListNode(6, None))))
+print("#21\t", end = ' ')
 while merged:
     print(merged.val, end =' ')
     merged = merged.next
@@ -57,6 +63,7 @@ print()
 
 # [LEETCODE #206 REVERSE LINKED LIST]
 reversed = solution.reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, None))))))
+print("#206\t", end = ' ')
 while reversed:
     print(reversed.val, end = ' ')
     reversed = reversed.next
@@ -65,6 +72,7 @@ print()
 
 # [LEETCODE #24 SWAP NODES IN PAIRS]
 swapped = solution.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4, None)))))
+print("#24\t", end = ' ')
 while swapped:
     print(swapped.val, end = ' ')
     swapped = swapped.next
@@ -73,6 +81,7 @@ print()
 
 # [LEETCODE #328 ODD EVEN LINKED LIST]
 odd_even = solution.oddEvenList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6, None)))))))
+print("#328\t", end = ' ')
 while odd_even:
 	print(odd_even.val, end = ' ') 
 	odd_even = odd_even.next 
@@ -81,6 +90,7 @@ print()
 
 # [LEETCODE #92 REVERSE LINKED LIST(2)]
 reversed = solution.reverseBetween(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6, None)))))), left = 2, right = 5)
+print("#92\t", end = ' ')
 while reversed: 
 	print(reversed.val, end = ' ') 
 	reversed = reversed.next 
@@ -88,17 +98,18 @@ while reversed:
 print()
 
 # [LEETCODE #20 VALID PARENTHESES]
-print(solution.isValidParentheses('()[]'))
+print("#20\t", solution.isValidParentheses('()[]'))
 
 # [LEETCODE #316 REMOVE DUPLICATE LETTERS]
-print(solution.removeDuplicateLetters('bcabc'))
+print("#316\t", solution.removeDuplicateLetters('bcabc'))
 
 # [LEETCODE #739 DAILY TEMPERATURES]
-print(solution.dailyTemperatures([73, 74, 75, 83, 79, 78, 77, 80]))
+print("#739\t", solution.dailyTemperatures([73, 74, 75, 83, 79, 78, 77, 80]))
 
 # [LEETCODE #23 MERGE K SORTED LISTS]
 
 mergedKLists = solution.mergeKLists([ListNode(1, ListNode(4, ListNode(5, None))), ListNode(1, ListNode(3, ListNode(4, None))), ListNode(2, ListNode(6, None))])
+print("#23\t", end =' ')
 while mergedKLists: 
 	print(mergedKLists.val, end = ' ') 
 	mergedKLists = mergedKLists.next
@@ -106,48 +117,71 @@ while mergedKLists:
 print() 
 
 # [BIRTHDAY PROBLEM]
+print("Birthday Problem\t", end = ' ')
 solution.birthdayProblem()
 
 # [LEETCODE #771 JEWELS AND STONES]
-print(solution.numJewelsInStones(jewels = "aA", stones = "aAAbbBBbb")) 
+print("#771\t", solution.numJewelsInStones(jewels = "aA", stones = "aAAbbBBbb")) 
 
 # [LEETCODE #3 LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS]
-print(solution.lengthOfLongestSubstring("aabbcabdbddbc")) 
+print("#3\t", solution.lengthOfLongestSubstring("aabbcabdbddbc")) 
 
 # [LEETCODE #347 TOP K FREQUENT ELEMENTS]
-print(solution.topKFrequent([1, 1, 1, 2, 2, 3], k = 2))
+print("#347\t", solution.topKFrequent([1, 1, 1, 2, 2, 3], k = 2))
 
 # [LEETCODE #200 NUMBER OF ISLANDS]  
-print(solution.numIslands(grid = [
+print("#200\t", solution.numIslands(grid = [
   ["1","1","1","1","0"],
   ["1","1","0","1","0"],
   ["1","1","0","0","1"],
   ["0","0","1","0","1"]]))
 
 # [LEETCODE #17 LETTER COMBINATIONS OF A PHONE NUMBER] 
-print(solution.letterCombinations("23"))
+print("#17\t", solution.letterCombinations("23"))
 
 # [LEETCODE #46 PERMUTATION]
-print(solution.permute([1, 2, 3]))
+print("#46\t", solution.permute([1, 2, 3]))
 
 # [LEETCODE #77 COMBINATION] 
-print(solution.combine(4, 2))
+print("#77\t", solution.combine(4, 2))
 
 # [LEETCODE #39 COMBINATION SUM] 
-print(solution.combinationSum(candidates = [2,3,6,7], target = 7))
+print("#39\t", solution.combinationSum(candidates = [2,3,6,7], target = 7))
 
 # [LEETCODE #78 SUBSETS]
-print(solution.subsets(nums = [1, 2, 5]))
+print("#78\t", solution.subsets(nums = [1, 2, 5]))
 
 # [LEETCODE #332 RECONSCRUCT ITINERARY] 
-print(solution.findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
+print("#332\t", solution.findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
 
 # [LEETCODE #207 COURSE SCHEDULE]
-print(solution.canFinish(5, prerequisites = [[1,4], [2,4], [3, 1], [3, 2]]))
+print("#207\t", solution.canFinish(5, prerequisites = [[1,4], [2,4], [3, 1], [3, 2]]))
 
 # [LEETCODE #743 NETWORK DELAY TIME]
-print(solution.networkDelayTime(times = [[2, 1, 1], [2, 6, 2], [2, 3, 2], [1, 3, 3], [6, 5, 2], [5, 3, 3], [3, 4, 4], [3, 7, 3]],n = 7, k = 2))
+print("#743\t", solution.networkDelayTime(times = [[2, 1, 1], [2, 6, 2], [2, 3, 2], [1, 3, 3], [6, 5, 2], [5, 3, 3], [3, 4, 4], [3, 7, 3]],n = 7, k = 2))
 
 # [LEETCODE #787 CHEAPEST FLIGHTS WITHIN K STOPS] 
-print(solution.findCheapestPrice(n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1)) 
-print(solution.findCheapestPrice(n = 4, flights = [[0,1,1], [1,2,1], [0,2,5],[2,3,1]], src = 0, dst = 3, k = 1))
+print("#787\t", solution.findCheapestPrice(n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1)) 
+print("#787\t", solution.findCheapestPrice(n = 4, flights = [[0,1,1], [1,2,1], [0,2,5],[2,3,1]], src = 0, dst = 3, k = 1))
+
+# [LEETCODE #104 MAXIMUM DEPTH OF BINARY TREE] 
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15) 
+root.right.left.left = TreeNode(30)
+root.right.left.right = TreeNode(45)
+root.right.left.right.right = TreeNode(60)
+root.right.right = TreeNode(7)
+
+print("#104\t", solution.maxDepth(root))
+print("#104\t", solution.maxDepthRecursive(root))
+
+# [LEETCODE #543 DIAMETER OF BINARY TREE] 
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right = TreeNode(3)
+
+print("#543\t", solution.diameterOfBinaryTree(root))
